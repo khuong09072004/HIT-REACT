212ed94ba3862728961f5b11f41d37a3c82cd13d
khuong09072004 <148343430+khuong09072004@users.noreply.github.com>
2024-03-11 17:31:16 +0700

    btvnb1

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import Banner from '../src/components/Banner/banner'
+import Banner from './components/Banner/banner.jsx';
+import "../src/index.css"
+import Header from './components/Header/header.jsx';
+import Content from './components/Content/content.jsx';
+import  Footer from './components/Footer/footer.jsx'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header />
     <Banner />
+    <Content />
+    <Footer />
   </React.StrictMode>,
 )
