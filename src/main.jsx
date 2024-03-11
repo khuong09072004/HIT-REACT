f3ea9202994b64cb9b79e7e7cd716ca524463490
khuong09072004 <148343430+khuong09072004@users.noreply.github.com>
2024-03-11 14:35:16 +0700

    btvn

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Banner from '../src/components/Banner/banner'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Banner />
+  </React.StrictMode>,
+)
